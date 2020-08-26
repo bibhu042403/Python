2295ba71a04ba404f3decf9fcde92c9bff9c1fd1
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-27 00:09:44 +0530

    formate programe

---
+++ format.py
@@ -0,0 +1,8 @@
+print('the {} {} {}'.format('fox','brown','quick'))
+print('the {2} {1} {0}'.format('fox','brown','quick'))
+print('the {f} {b} {q}'.format(f='fox',b='brown',q='quick'))
+result=100/777
+print(result)
+print("the result was {r:1.3f}".format(r=result))
+print("the result was {r:1.5f}".format(r=result))
+print("the result was {r:10.3f}".format(r=result))
