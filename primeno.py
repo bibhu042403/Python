7a1970ca790372cfef48b8aaf3f004b265b7f6cc
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-26 13:51:36 +0530

    small change

--- primeno.py
+++ primeno.py
@@ -2,6 +2,7 @@ lower = 900
 upper = 1200
 
 print("Prime numbers between", lower, "and", upper, "are:")
+print("hello")
 for num in range(lower, upper + 1):
    # all prime numbers are greater than 1
    if num > 1:
