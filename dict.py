394470ab5ffdf8ca8067392439dfecb245537141
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-27 13:25:13 +0530

    disctionary problem

---
+++ dict.py
@@ -0,0 +1,16 @@
+my_dict={'key1':'value1','key2':'value2'}
+print(my_dict)
+print(my_dict['key1'])
+print(my_dict['key2'])
+f_price={'apple':230,'mango':120,'grapes':150}
+print(f_price['apple'])
+d={'k1':230,'k2':[0,1,2],'k3':{'insidekey':100}}
+print(d['k1'])
+print(d['k2'])
+print(d['k2'][2])
+print(d['k3'])
+print(d['k3']['insidekey'])
+my_dict['key3']=300
+print(my_dict)
+print(my_dict.values())
+
