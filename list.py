a85e93f5da94a1c0f23b0dc0e7e41bc1ce86e866
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-27 12:37:07 +0530

    basic list

---
+++ list.py
@@ -0,0 +1,13 @@
+mylist=['one','two','three','four']
+print(mylist[0])
+print(mylist[1:])
+anlist = ['five','six']
+mylisy = mylist.append(anlist[1])
+print(mylist)
+print(mylist.pop())
+print(mylist.pop(1))
+print(mylist.sort())
+
+
+
+
