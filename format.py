52a774530723471c835feedeeb38c97778c70584
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-31 21:00:21 +0530

    basic while loop problems

--- format.py
+++ format.py
@@ -3,6 +3,7 @@ print('the {2} {1} {0}'.format('fox','brown','quick'))
 print('the {f} {b} {q}'.format(f='fox',b='brown',q='quick'))
 result=100/777
 print(result)
+print('bibhu')
 print("the result was {r:1.3f}".format(r=result))
 print("the result was {r:1.5f}".format(r=result))
 print("the result was {r:10.3f}".format(r=result))
