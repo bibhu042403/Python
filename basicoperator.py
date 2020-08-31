9ee5780ab8dea1ecd5e9046c26023bcdd6390b58
bibhu042403 <56951899+bibhu042403@users.noreply.github.com>
2020-08-31 21:22:59 +0530

    basic while loop problems

---
+++ basicoperator.py
@@ -0,0 +1,13 @@
+from random import randint
+randm_no = randint(0,100)
+print(randm_no)
+print('\n')
+# printing the random number between 1 and 1000.
+r_no=randint(1,1000)
+print(r_no)
+print('\n')
+nm=input("Enter some number")
+print(nm)# this number will be string
+print(type(nm))
+print(float(nm))
+print(int(nm))
--- list.py
+++ list.py
@@ -16,6 +16,25 @@ from random import shuffle
 mylist12=[1,2,3,4,5,6,7,8,9]
 shuffle(mylist12)
 print(mylist12)
+print('\n')
+
+mlist='hello'
+mlist1=[]
+for letter in mlist:
+    mlist1.append(letter)
+print(mlist1)
+
+# Another method to print same things
+mlist2=[letter for letter in mlist]
+print(mlist2)
+mlist3=[x for x in 'Hello']
+print(mlist3)
+mlist4=[y for y in range(0,10)]
+print(mlist4)
+mlist5=[z**2 for z in range(1,10)]
+print(mlist5)
+mlist6=[n for n in range(0,10) if n%2==0]
+print(mlist6)
 
 
 
